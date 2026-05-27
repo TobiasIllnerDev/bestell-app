f32eb620cb3a4f0b99d850994651b464a3d96bb9
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-05-27 17:48:59 +0200

    Added Salad to array

--- scripts/db.js
+++ scripts/db.js
@@ -58,4 +58,34 @@ const menu = [
             }
         ]
     }
+
+    {
+        category: "Salad",
+        products: [
+            {
+                name: "Warm beef arugula salad",
+                price: 16.90,
+                description: "Beef, Arugula,Field salad,Greek feta,Cherry tomatoes,Sun-dried Tomatoes,Balsamic-vinegar dressing",
+                image: ""
+            },
+            {
+                name: "Mini green Salad",
+                price: 7.90,
+                description: "Green salad,Cucumber,Carrots,Parsley,Radishes ",
+                image: ""
+            },
+            {
+                name: "Green Salad with sea food",
+                price: 16.90,
+                description: "Mixed greens,Cherry tomatoes,Red onion,Mussels,Squid rings,Shrimp,Dijon mustard-lemon dressing with dill",
+                image: ""
+            },
+            {
+                name: "Vegan green salad with tofu",
+                price: 14.90,
+                description: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+                image: ""
+            }
+        ]
+    }
 ];
