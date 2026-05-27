f238b56fbec314d2b2fbeaf63043add05eb54c86
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-05-27 17:38:33 +0200

    Added Array with burger

---
+++ scripts/db.js
@@ -0,0 +1,43 @@
+const menu = [
+    {
+        category: "Burger",
+        products: [
+            {
+                name: "Veggie mushroom black burger",
+                price: 16.90,
+                description: "Mixed green salad,Tomatoesm,Edamame,Mushrooms",
+                image: "./img/burger.jpg"
+            },
+            {
+                name: "All meat burger",
+                price: 15.90,
+                description: "Beef,Bacon,Dill pickles,Smoked cheese,Ketchup,BBQ souse",
+                image: "./img/chicken.jpg"
+            },
+            {
+                name: "Beef red burger",
+                price: 14.90,
+                description: "Beef,Cheese,Tomatoes,Lettuce,Onion",
+                image: "/assets/imgs"
+            },
+            {
+                name: "Big chicken burger",
+                price: 15.90,
+                description: "Chicken,Cheese,Tomatoes,Lettuce,Onion,Bell pepper",
+                image: "/assets/imgs"
+            },
+        ]
+    },
+
+    {
+        category: "Pizza",
+        products: [
+            {
+                name: "Margherita",
+                price: 9.90,
+                description: "Tomate und Mozzarella",
+                image: "./img/pizza.jpg"
+            }
+        ]
+    }
+];
